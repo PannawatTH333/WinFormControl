878eb563bb7d263adedbc940977aea3e27bd01d4
PannawatTH333 <Plamthailand2546@gmail.com>
2024-11-18 17:13:22 +0700

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (label1.Text == "มหาวิทยาลัยราชภัฎอุดรธานี")
+            {
+                label1.Text = "Udonthani Rajabhat University";
+            }
+            else
+            {
+                label1.Text = "มหาวิทยาลัยราชภัฎอุดรธานี";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text;
+
+            Random rn = new Random();
+            int r = rn.Next(0, 255);
+            int g = rn.Next(0, 255);
+            int b = rn.Next(0, 255);
+            label1.ForeColor = Color.FromArgb(r, g, b);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.LightYellow;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.DarkGray;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello, User", "First Message");
+            //MessageBox.Show("Hello, User", "Secand Message",MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+        }
+    }
+}
